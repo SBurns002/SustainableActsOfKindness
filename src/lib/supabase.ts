ea03b0c4f56f193fa14c5016d038a046b2b26e21
SBurns002 <sdavies8@bu.edu>
2025-06-03 20:35:14 -0400

    Configure Supabase Connection

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://kxcuiyvxxvylackjjlgn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4Y3VpeXZ4eHZ5bGFja2pqbGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2ODI0MDAsImV4cCI6MjAyNTI1ODQwMH0.Wd0VZPPOEt_Kf_QZCqPHXKGZHoNi7KBg5UHyAqXtpHw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
